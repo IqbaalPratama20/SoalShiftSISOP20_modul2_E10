9708b3cb240f73f64a71f3a768a4a0bf90d10c09
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2020-03-11 17:11:12 +0700

    yok upload yok

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -6,23 +6,18 @@
 #include <time.h>
 #include <string.h>
 
-void tostring(char str[], int num)
-{
-    int i, rem, len = 0, n;
- 
-    n = num;
-    while (n != 0)
-    {
-        len++;
-        n /= 10;
-    }
-    for (i = 0; i < len; i++)
-    {
-        rem = num % 10;
-        num = num / 10;
-        str[len - (i + 1)] = rem + '0';
-    }
-    str[len] = '\0';
+char* itoa(int val, int base){
+	
+	static char buf[32] = {0};
+	
+	int i = 30;
+	
+	for(; val && i ; --i, val /= base)
+	
+		buf[i] = "0123456789abcdef"[val % base];
+	
+	return &buf[i+1];
+	
 }
 
 void subv(char *path, char * const argv[])
@@ -64,40 +59,32 @@ int main()
     else 
     {
         wait(0);
+        for(int i=0;i<2;i++)
+        {
+            char a[50];
+            char str[100];
+            char web[100];
+            time(&rawtime);
+            info = localtime(&rawtime);
+            strftime(a,50,"%Y-%m-%d_%X", info);
+            char b[80];
+            strcpy(b,"/home/iqbaal/");
+            strcat(b,folder);
+            strcat(b,"/");
+            strcat(b, a);
+            int waktu = (time(0)%1000) + 100;
+            strcpy(str, itoa(waktu, 10));
+            strcpy(web,"https://picsum.photos/");
+            strcat(web, str);
+            char *args[] = {"wget", "-O",b, web, NULL};
+            subv("/usr/bin/wget", args);
+            // sleep(5);
+        }
         char asiap[50];
-        strcpy(asiap, dest);
-        strcat(asiap,".zip");
-        char *args[] = {"zip", "-O", asiap, dest, NULL};
-        execv("usr/bin/zip", args);
-        // for(int i=0;i<2;i++)
-        // {
-        //     char a[50];
-        //     char str[100];
-        //     char web[100];
-        //     time(&rawtime);
-        //     info = localtime(&rawtime);
-        //     strftime(a,50,"%Y-%m-%d_%X", info);
-        //     char b[80];
-        //     strcpy(b,"/home/iqbaal/");
-        //     strcat(b,folder);
-        //     strcat(b,"/");
-        //     strcat(b, a);
-        //     int waktu = (time(0)%1000) + 100;
-        //     tostring(str,waktu);
-        //     strcpy(web,"https://picsum.photos/");
-        //     strcat(web, str);
-        //     char *args[] = {"wget", "-O",b, web, NULL};
-        //     subv("/usr/bin/wget", args);
-        //     if(i==1)
-        //     {
-        //         char asiap[50];
-        //         strcpy(asiap, dest);
-        //         strcat(asiap,".zip");
-        //         char *args[] = {"zip", dest, NULL};
-        //         subv("usr/bin/zip", args);
-        //     }
-        //     sleep(5);
-        // }
-        
+        strcat(asiap, dest);
+        char *args[] = {"zip", "-r", dest, dest, NULL};
+        char *arglp[]= { "rm", "-r", dest, NULL};
+        subv("/usr/bin/zip", args);
+        subv("/bin/rm", arglp);
     }
 }
