cf77a3dc9a92ef85bf6ca55010ec761252b0f020
iqbaaaaal <57170373+IqbaalPratama20@users.noreply.github.com>
2020-03-12 15:27:43 +0700

    oiii

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -10,6 +10,19 @@
 #include <regex.h>
 #include <time.h>
 
+
+void subv(char *path, char * const argv[])
+{
+    int ret;
+    pid_t a_pid = fork();
+    if (a_pid == -1) return;
+    if (a_pid != 0) 
+    { 
+        wait(&ret); 
+        return; 
+    }
+    execv(path, argv);
+}
 void service_maker(char * second, char * minutes, char * hour, char * path_file){
     // pid_t pid, sid;        // Variabel untuk menyimpan PID
     // pid = fork();     // Menyimpan PID dari Child Process
@@ -80,29 +93,105 @@ int pattern_check(char * paths){
 }
 
 
-int main(int argc, char **argv){
+int main(){
     //printf("%d", argc);
-    if (argc-1 > 4){
-        printf("Terjadi kesalahan input\n");
-    }else{
-        if ( (strcmp(argv[1], "*") == 0) || (strcmp(argv[1], "0") >= 0 && strcmp(argv[1],"59") <= 0) ){
-            if ( (strcmp(argv[2], "*") == 0) || (strcmp(argv[2], "0") >= 0 && strcmp(argv[2],"59") <= 0) ){
-                if ( (strcmp(argv[3], "*") == 0) || (strcmp(argv[3], "0") >= 0 && strcmp(argv[3],"23") <= 0) ){
-                    if (pattern_check(argv[4]) == 1){
-                        service_maker(argv[1], argv[2], argv[3], argv[4]);
-                    }else{
-                        printf("Terjadi kesalahan pada argumen 4\n");    
-                    }
-                }else{
-                    printf("Terjadi kesalahan pada argumen 3\n");    
+    int ceksalah=1;
+    // if (argc-1 > 4){
+    //     printf("Terjadi kesalahan input\n");
+    // }else{
+    //     if ( (strcmp(argv[1], "*") == 0) || (strcmp(argv[1], "0") >= 0 && strcmp(argv[1],"59") <= 0) ){
+    //         if ( (strcmp(argv[2], "*") == 0) || (strcmp(argv[2], "0") >= 0 && strcmp(argv[2],"59") <= 0) ){
+    //             if ( (strcmp(argv[3], "*") == 0) || (strcmp(argv[3], "0") >= 0 && strcmp(argv[3],"23") <= 0) ){
+    //                 if (pattern_check(argv[4]) == 1){
+    //                     service_maker(argv[1], argv[2], argv[3], argv[4]);
+    //                 }else{
+    //                     ceksalah=1;
+    //                     printf("Terjadi kesalahan pada argumen 4\n");    
+    //                 }
+    //             }else{
+    //                 ceksalah=1;
+    //                 printf("Terjadi kesalahan pada argumen 3\n");    
+    //             }
+    //         }else{
+    //             ceksalah=1;
+    //             printf("Terjadi kesalahan pada argumen 2\n");    
+    //         }
+    //     }else{
+    //         ceksalah=1;
+    //         printf("Terjadi kesalahan pada argumen 1\n");
+    //     }
+    // }
+    while(ceksalah!=2)
+    {
+        time_t times;
+        struct tm* tm_info;  
+        times = time(NULL);
+        tm_info = localtime(&times);
+        char skrg_jam[5];
+        sprintf(skrg_jam,"%d", tm_info->tm_hour);
+        char skrg_menit[5]; 
+        sprintf(skrg_menit, "%d", tm_info->tm_min);
+        char skrg_detik[5];
+        sprintf(skrg_detik, "%d", tm_info->tm_sec);
+        if( strcmp(arg[3], "*")==0)
+        {
+            if(strcmp(arg[2],"*") == 0)
+            {
+                if(strcmp(arg[1],"*")==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
                 }
-            }else{
-                printf("Terjadi kesalahan pada argumen 2\n");    
+                else if(strcmp(skrg_detik,arg[1])==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
             }
-        }else{
-            printf("Terjadi kesalahan pada argumen 1\n");
+            else if(strcmp(skrg_menit, arg[2]))
+            {
+                if(strcmp(arg[1],"*")==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+                else if(strcmp(skrg_detik,arg[1])==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+            }
         }
-    }
+        else if( strcmp(arg[3],skrg_jam)==0)
+        {
+            if(strcmp(arg[2],"*") == 0)
+            {
+                if(strcmp(arg[1],"*")==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+                else if(strcmp(skrg_detik,arg[1])==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+            }
+            else if(strcmp(skrg_menit, arg[2])==0)
+            {
+                if(strcmp(arg[1],"*")==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+                else if(strcmp(skrg_detik,arg[1])==0)
+                {
+                    char *args[] = {"bash", arg[4], NULL};
+                    subv("/bin/bash", args);
+                }
+            }
+        }
+        sleep(1);
     
     return 0;
 }
